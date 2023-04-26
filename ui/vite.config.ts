eb824fdb62f65f47cd52e45e9c8d28c9389ec62c
Bug Lover Journey <bugloverjourney@gmail.com>
2023-04-26 19:39:43 +0200

    UI: import alias resolve

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@const': path.resolve(__dirname, './src/api/constants'),
+      '@hook': path.resolve(__dirname, './src/hooks'),
+      '@comp': path.resolve(__dirname, './src/components'),
+      '@dto': path.resolve(__dirname, './src/api/dto'),
+    },
+  }
 })
