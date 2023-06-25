8876a70e7f9124ea9bdb82535010135a2a104a3b
Bug Lover Journey <bugloverjourney@gmail.com>
2023-06-25 19:25:33 +0200

    UI: Project page changes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Root from './pages/Root';
-import Projects from './pages/Projects';
+import Projects from './pages/projects/Projects';
 import About from "./pages/About";
 import BasePage from "./pages/BasePage";
 
@@ -15,9 +15,9 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
-        <Route index path="Home" element={<BasePage comp={<Home/>}/>} />
-        <Route  path="About" element={<BasePage comp={<About/>}/>} />
-        <Route  path="Projects" element={<BasePage comp={<Projects/>}/>} />
+        <Route index path="Home" element={<BasePage comp={<Home />} />} />
+        <Route path="About" element={<BasePage comp={<About />} />} />
+        <Route path="Projects" element={<BasePage comp={<Projects />} />} />
       </Route>
     )
   );
