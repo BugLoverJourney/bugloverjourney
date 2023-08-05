c41b38e9d759246574e780c1c6f2ad01d14e62b3
Bug Lover Journey <bugloverjourney@gmail.com>
2023-08-05 18:45:11 +0200

    BE: sensitive info to ENV

--- server/index.php
+++ server/index.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-spl_autoload_register("myAutoLoader");
+spl_autoload_register('myAutoLoader');
 
 function myAutoLoader (string $class) {
-  if (str_contains($class, "Controller")) {
+  if (str_contains($class, 'Controller')) {
     require "./src/controllers/$class.php";
 
     return;
   }
 
-  if (str_contains($class, "Gateway")) {
+  if (str_contains($class, 'Gateway')) {
     require "./src/gateways/$class.php";
 
     return;
@@ -20,26 +20,33 @@ function myAutoLoader (string $class) {
   require "./src/$class.php";
 }
 
-set_error_handler("ErrorHandler::handleError");
-set_exception_handler("ErrorHandler::handleException");
+set_error_handler('ErrorHandler::handleError');
+set_exception_handler('ErrorHandler::handleException');
 
 function errorCall(int $statusCode, string $msg) {
   (new ErrorController($statusCode, $msg))->response();
   exit;
 }
 
-header("Content-type: application/json");
+header('Content-type: application/json');
 
-$url = explode("/", $_SERVER["REQUEST_URI"]);
+$url = explode('/', $_SERVER['REQUEST_URI']);
 
-if ($url[1] !== "api") {
-  errorCall(404, "Unpredicted API call"); 
-} 
+if ($url[1] !== 'api') {
+  errorCall(404, 'Unpredicted API call'); 
+}
+
+require_once realpath(__DIR__ . '/../../vendor/autoload.php');
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
 switch ($url[2]) {
-  case "users":
+  case 'users':
     (new UsersController())->response();
     break;
   default:
-    errorCall(404, "Called API resource doesn`t exist!");
+    errorCall(404, 'Called API resource doesn`t exist!');
 }
--- server/src/controllers/BaseController.php
+++ server/src/controllers/BaseController.php
@@ -8,7 +8,7 @@ class BaseController {
   protected function __construct() {
     $this->method = $_SERVER['REQUEST_METHOD'];
     $this->url = explode('/', $_SERVER['REQUEST_URI']);
-    $this->db = new Database('localhost','bugLoverJourney','root','');
+    $this->db = new Database($_ENV['DB_HOST'],$_ENV['DB_NAME'],$_ENV['DB_USER'],$_ENV['DB_PWD']);
   }
 
   private static function process($res): void {
